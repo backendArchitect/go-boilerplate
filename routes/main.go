49267f87f1c04bd63458064219c44a404aa345df
Vatsal Chauhan <87218847+codeArtisanry@users.noreply.github.com>
2024-08-20 10:18:58 +0530

    feat: Add utils and routes

---
+++ routes/main.go
@@ -0,0 +1,52 @@
+package routes
+
+import (
+	"database/sql"
+	"sync"
+
+	"go.uber.org/zap"
+
+	"github.com/codeArtisanry/go-boilerplate/config"
+	controller "github.com/codeArtisanry/go-boilerplate/controllers/api/v1"
+
+	"github.com/codeArtisanry/go-boilerplate/middlewares"
+	"github.com/gofiber/fiber/v2"
+)
+
+var mu sync.Mutex
+
+// Setup func
+func Setup(app *fiber.App, db *sql.DB, logger *zap.Logger, config config.AppConfig) error {
+	mu.Lock()
+
+	app.Use(middlewares.LogHandler(logger))
+
+	app.Static("/assets/", "./assets")
+
+	app.Get("/docs", func(c *fiber.Ctx) error {
+		return c.Render("./assets/index.html", fiber.Map{})
+	})
+
+	router := app.Group("/api")
+	v1 := router.Group("/v1")
+
+	middlewares := middlewares.NewMiddleware(config, logger)
+
+	err := setupUserController(v1, db, logger, config, middlewares)
+	if err != nil {
+		return err
+	}
+
+	mu.Unlock()
+	return nil
+}
+
+func setupUserController(router fiber.Router, db *sql.DB, logger *zap.Logger, config config.AppConfig, middlewares middlewares.Middleware) error {
+	userController, err := controller.NewUserController(db, logger, config)
+	if err != nil {
+		return err
+	}
+
+	router.Post("/users", userController.Create)
+	return nil
+}
