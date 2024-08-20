67a1e574d86a6bb1d0bff45adb728c9b8bf85d6c
Vatsal Chauhan <87218847+codeArtisanry@users.noreply.github.com>
2024-08-20 10:14:44 +0530

    chore: Add constants package for storing application constants

---
+++ constants/main.go
@@ -0,0 +1,35 @@
+package constants
+
+// variables
+const (
+	DateTimeFormat   string = "2006-01-02T15:04:05Z"
+	PAGINATION_LIMIT int32  = 5
+	OPEN_STATUS      string = "OPEN"
+	CLOSED_STATUS    string = "CLOSED"
+	MERGED_STATUS    string = "MERGED"
+	PR_ALL_STATUS    string = "OPEN,CLOSED,MERGED"
+	ISSUE_ALL_STATUS string = "OPEN,CLOSED"
+)
+
+// fiber contexts
+const ()
+
+// params
+const (
+	ParamOid    string = "OrgID"
+	ParamStatus string = "Status"
+	ParamOrg    string = "organization"
+	ParamRepo   string = "repository"
+	ParamMember string = "member"
+)
+
+// Success messages
+// ...
+const ()
+
+// Fail messages
+// ...
+const ()
+
+// Error messages
+const ()
